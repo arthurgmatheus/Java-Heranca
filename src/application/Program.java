502c3fc6458e7a95c3e3e9b595968357bb96c023
Arthur Matheus <arthurgm2002@icloud.com>
2023-06-03 20:50:55 -0300

    alteraçao

---
+++ src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Account;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+        Account account = new Account();
+
+        account.getBalance();
+
+	}
+
+}
